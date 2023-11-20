fa3773c517cbe18c627c0759929a64c11f2f2107
mrsupercraft <itamarha2006@gmail.com>
2023-11-20 19:41:02 +0200

    added smooth scrolling w jQuery

--- MrSuperCraft/src/scripts/smoothScroll.js
+++ MrSuperCraft/src/scripts/smoothScroll.js
@@ -1,23 +1,30 @@
-$(document).ready(function(){
-  
-    $("a").on('click', function(event) {
-  
-     
-      if (this.hash !== "") {
-     
-        event.preventDefault();
-  
-    
-        var hash = this.hash;
-  
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 800, function(){
-  
-       
+// Importing jQuery
+
+// Plain JavaScript equivalent of $(document).ready()
+document.addEventListener("DOMContentLoaded", function () {
+  $("a").on("click", function (event) {
+    // Explicitly cast 'this' to HTMLAnchorElement
+    var anchor = this;
+
+    // Make sure anchor.hash has a value before overriding default behavior
+    if (anchor.hash !== undefined && anchor.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = anchor.hash;
+
+      // Using jQuery's animate() method for smooth page scroll
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800,
+        function () {
+          // Add hash (#) to URL when done scrolling (default click behavior)
           window.location.hash = hash;
-        });
-      } 
-    });
+        }
+      );
+    }
   });
-  
+});
