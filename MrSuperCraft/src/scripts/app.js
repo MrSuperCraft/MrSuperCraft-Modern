f7dece02a3f5e4a8a683bc5fd0e17d13b7c141fd
mrsupercraft <itamarha2006@gmail.com>
2023-11-21 21:18:52 +0200

    update improved theme script

--- MrSuperCraft/src/scripts/app.js
+++ MrSuperCraft/src/scripts/app.js
@@ -1,30 +1,35 @@
 document.addEventListener("DOMContentLoaded", function () {
+    const themeToggleBtn = document.getElementById("theme-toggle");
     const sunIcon = document.getElementById("theme-toggle-dark-icon");
     const moonIcon = document.getElementById("theme-toggle-light-icon");
-
-    let userTheme = localStorage.getItem("theme") || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
-
+  
+    const userTheme = localStorage.getItem("theme");
+    const systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  
     const iconToggle = () => {
-      sunIcon.classList.toggle("hidden", userTheme !== "dark");
-      moonIcon.classList.toggle("hidden", userTheme === "dark");
+      sunIcon.classList.toggle("hidden", !document.documentElement.classList.contains("dark"));
+      moonIcon.classList.toggle("hidden", document.documentElement.classList.contains("dark"));
     };
-
+  
     // Initial Theme Check
     const themeCheck = () => {
-      document.documentElement.classList.toggle("dark", userTheme === "dark");
+      if (userTheme === "dark" || (!userTheme && systemTheme)) {
+        document.documentElement.classList.add("dark");
+      }
       iconToggle(); // Show/hide icons based on the initial theme
     };
-
+  
     // Manual Theme Switch
     const themeSwitch = () => {
-      userTheme = (userTheme === "dark") ? "light" : "dark";
-      localStorage.setItem("theme", userTheme);
-      themeCheck();
+      document.documentElement.classList.toggle("dark");
+      const newTheme = document.documentElement.classList.contains("dark") ? "dark" : "light";
+      localStorage.setItem("theme", newTheme);
+      iconToggle();
     };
-
-    sunIcon.addEventListener("click", themeSwitch);
-    moonIcon.addEventListener("click", themeSwitch);
-
+  
+    themeToggleBtn.addEventListener("click", themeSwitch); // Use the button for theme switch
+  
     // Invoke themeCheck on initial load
     themeCheck();
   });
+  
