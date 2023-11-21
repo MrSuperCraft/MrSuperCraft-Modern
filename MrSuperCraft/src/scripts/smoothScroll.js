50cd454203cfcf4f25920ffc8ddbb9ba5daf8e7e
mrsupercraft <itamarha2006@gmail.com>
2023-11-21 20:56:49 +0200

    update smooth scroll algorithm

--- MrSuperCraft/src/scripts/smoothScroll.js
+++ MrSuperCraft/src/scripts/smoothScroll.js
@@ -1,30 +1,37 @@
-// Importing jQuery
-
-// Plain JavaScript equivalent of $(document).ready()
 document.addEventListener("DOMContentLoaded", function () {
-  $("a").on("click", function (event) {
-    // Explicitly cast 'this' to HTMLAnchorElement
-    var anchor = this;
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (event) {
+      var target = document.querySelector(this.getAttribute('href'));
 
-    // Make sure anchor.hash has a value before overriding default behavior
-    if (anchor.hash !== undefined && anchor.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
+      if (target) {
+        event.preventDefault();
 
-      // Store hash
-      var hash = anchor.hash;
+        var hash = this.hash;
+        var targetPosition = target.getBoundingClientRect().top + window.scrollY;
+        var startPosition = window.pageYOffset;
+        var distance = targetPosition - startPosition;
+        var startTime = null;
 
-      // Using jQuery's animate() method for smooth page scroll
-      $("html, body").animate(
-        {
-          scrollTop: $(hash).offset().top,
-        },
-        800,
-        function () {
-          // Add hash (#) to URL when done scrolling (default click behavior)
-          window.location.hash = hash;
+        function animateScroll(timestamp) {
+          if (!startTime) startTime = timestamp;
+          var progress = timestamp - startTime;
+          var easing = function (t) {
+            return t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1;
+          };
+          var increment = easing(progress / 800) * distance;
+
+          window.scrollTo(0, startPosition + increment);
+
+          if (progress < 800) {
+            requestAnimationFrame(animateScroll);
+          } else {
+            window.location.hash = hash;
+          }
         }
-      );
-    }
+
+        requestAnimationFrame(animateScroll);
+      }
+    });
   });
 });
+
