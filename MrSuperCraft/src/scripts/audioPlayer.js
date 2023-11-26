6ab605f511b6171b7c58f8094ba92620b0dbc0f8
mrsupercraft <itamarha2006@gmail.com>
2023-11-26 18:39:38 +0200

    add modal cover view on click

--- MrSuperCraft/src/scripts/audioPlayer.js
+++ MrSuperCraft/src/scripts/audioPlayer.js
@@ -13,83 +13,97 @@ const audioList = [
     src: "../audio/1. Mind Palace.wav",
     coverArt: "../images/coverart/mindPalace.png",
     artistName: "MrSuperCraft, Brunch Collect",
+    releaseName: "Mind Palace",
     songTitle: "Mind Palace",
   },
   {
     src: "../audio/2. Midnight Contemplation.wav",
     coverArt: "../images/coverart/mindPalace.png",
     artistName: "MrSuperCraft, Brunch Collect",
+    releaseName: "Mind Palace",
     songTitle: "Midnight Contemplation",
   },
   {
     src: "../audio/3. Fireflies.wav",
     coverArt: "../images/coverart/mindPalace.png",
     artistName: "MrSuperCraft, Brunch Collect",
+    releaseName: "Mind Palace",
     songTitle: "Fireflies",
   },
   {
     src: "../audio/4. Solitude.wav",
     coverArt: "../images/coverart/mindPalace.png",
     artistName: "MrSuperCraft, Brunch Collect",
+    releaseName: "Mind Palace",
     songTitle: "Solitude",
   },
   {
     src: "../audio/5. Subconscious.wav",
     coverArt: "../images/coverart/mindPalace.png",
     artistName: "MrSuperCraft, Brunch Collect",
+    releaseName: "Mind Palace",
     songTitle: "Subconscious",
   },
   {
     src: "../audio/6. Mt. Imagination.wav",
     coverArt: "../images/coverart/mindPalace.png",
     artistName: "MrSuperCraft, Brunch Collect",
+    releaseName: "Mind Palace",
     songTitle: "Mt. Imagination",
   },
   {
     src: "../audio/1. Red Skies.wav",
     coverArt: "../images/coverart/SummerStories.png",
     artistName: "MrSuperCraft",
+    releaseName: "Summer Stories",
     songTitle: "Red Skies",
   },
   {
     src: "../audio/2. Dusk.wav",
     coverArt: "../images/coverart/SummerStories.png",
     artistName: "MrSuperCraft",
+    releaseName: "Summer Stories",
     songTitle: "Dusk",
   },
   {
     src: "../audio/3. Tears Grow Trees.wav",
     coverArt: "../images/coverart/SummerStories.png",
     artistName: "MrSuperCraft",
+    releaseName: "Summer Stories",
     songTitle: "Tears Grow Trees",
   },
   {
     src: "../audio/4. False Reality.wav",
     coverArt: "../images/coverart/SummerStories.png",
     artistName: "MrSuperCraft",
+    releaseName: "Summer Stories",
     songTitle: "False Reality",
   },
   {
     src: "../audio/5. Heart's Truth.wav",
     coverArt: "../images/coverart/SummerStories.png",
     artistName: "MrSuperCraft",
+    releaseName: "Summer Stories",
     songTitle: "Heart's Truth",
   },
   {
     src: "../audio/6. Was then Wasn't.wav",
     coverArt: "../images/coverart/SummerStories.png",
     artistName: "MrSuperCraft",
+    releaseName: "Summer Stories",
     songTitle: "Was then Wasn't",
   },
   {
     src: "../audio/7. The End is only The Beginning of Something New.wav",
     coverArt: "../images/coverart/SummerStories.png",
     artistName: "MrSuperCraft",
+    releaseName: "Summer Stories",
     songTitle: "The End is only The Beginning of Something New",
   },
   // Add more audio file and cover art objects as needed
 ];
 
+// Shuffling Songs
 let currentAudioIndex = 0;
 let isShuffleMode = false;
 let shuffledIndices = Array.from({ length: audioList.length }, (_, index) => index);
@@ -175,3 +189,45 @@ progressSlider.addEventListener('mouseover', () => {
 progressSlider.addEventListener('mouseout', () => {
   progressSlider.classList.add('thumb-hidden');
 });
+
+
+
+// Modal Opener / Closer
+
+// Function to open the modal
+
+const modal = document.getElementById("myModal");
+const modalCoverArt = document.getElementById("modal-cover-art");
+const modalDescription = document.getElementById("modal-description"); 
+const closeButton = document.querySelector(".close");
+
+
+function openModal() {
+  const currentReleaseName = audioList[currentAudioIndex].releaseName;
+  // Set modal content based on the active cover art
+  modalCoverArt.src = coverArt.src;
+  modalDescription.innerText = `From the release: ${currentReleaseName}`;
+  modal.classList.remove("hidden");
+}
+
+if (modalDescription && closeButton) {
+  const currentReleaseName = audioList[currentAudioIndex].releaseName;
+
+  // Set modal content based on the active cover art
+  modalCoverArt.src = coverArt.src;
+  modalDescription.innerText = `From the release: ${currentReleaseName}`;
+  
+}
+
+
+function closeModal() {
+  document.getElementById("myModal").classList.add("hidden");
+}
+
+// Event listener for opening the modal when cover art is clicked
+document.getElementById("cover-art")?.addEventListener("click", openModal);
+
+// Event listener for closing the modal when the close button is clicked
+document.querySelector(".close")?.addEventListener("click", closeModal);
+
+
