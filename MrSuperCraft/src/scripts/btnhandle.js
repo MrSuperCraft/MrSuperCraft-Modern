da847d94c9735299043ee9e5aaf71e941f10c195
mrsupercraft <itamarha2006@gmail.com>
2023-11-28 23:12:57 +0200

    debugging

--- MrSuperCraft/src/scripts/btnhandle.js
+++ MrSuperCraft/src/scripts/btnhandle.js
@@ -1,21 +1,22 @@
-import { releases } from "../scripts/songs";
-
+import { releases } from "./songs";
 
 // Get all elements with class "swiper-slide"
+document.addEventListener("DOMContentLoaded" , () => {
+    const swiperSlides = document.querySelectorAll('.swiper-slide');
 
-const swiperSlides = document.querySelectorAll('.swiper-slide');
-
-// Loop through each swiper slide
-    swiperSlides.forEach(function (slide, index) {
-        // Get the corresponding release data
-    const releaseData = releases[index];
-
-        // Update the href attributes based on the links array
-    releaseData.links.forEach(function (link, linkIndex) {
-        const releaseLink = slide.querySelectorAll('a')[linkIndex];
-        if (releaseLink) {
-            releaseLink.href = link;
-            releaseLink.target = "_blank";
-        }
-    });
+    // Loop through each swiper slide
+        swiperSlides.forEach(function (slide, index) {
+            // Get the corresponding release data
+        const releaseData = releases[index];
+    
+            // Update the href attributes based on the links array
+        releaseData.links.forEach(function (link, linkIndex) {
+            const releaseLink = slide.querySelectorAll('a')[linkIndex];
+            if (releaseLink) {
+                releaseLink.href = link;
+                releaseLink.target = "_blank";
+            }
+        })
+    })
 });
+
