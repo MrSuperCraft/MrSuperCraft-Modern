92f58230c1c9d2ec7d3020eb9c52bbf9afdb858e
mrsupercraft <itamarha2006@gmail.com>
2023-12-08 20:49:20 +0200

    config env vars

--- MrSuperCraft/astro.config.mjs
+++ MrSuperCraft/astro.config.mjs
@@ -10,9 +10,5 @@ export default defineConfig({
     "./src/components/**/*.astro",
     // ... other component paths
   ],
-  env: {
-    SPOTIFY_CLIENT_ID: '7a2b4b5ae2cb4a1484d54801e1c3871e',
-    SPOTIFY_CLIENT_SECRET: '4439117faebd4d2f9de5282e2c3fc668',
-  },
 });
 
